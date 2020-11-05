83a14b8f36ddb7f6f893e17b8441285408f21f8e
tomoheko <dekirumadeyamenai@gmail.com>
2020-11-05 19:38:31 +0900

    コンセプトの修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,11 +4,12 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+// require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("../product_price")
 require("../card")
+require('./preview')
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
