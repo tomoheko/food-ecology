e3e23ed87fa4912dd30637a9d0c083181e418f29
tomoheko <dekirumadeyamenai@gmail.com>
2020-11-06 16:46:44 +0900

    about/indexの編集、各ページの細部編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "products#index"
+  resources :abouts, only:[:index]
   resources :products do
     resources :orders, only:[:index, :create]
   end
