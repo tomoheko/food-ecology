78028aa417dd12f1238d2a7385eb7a8b451a094a
tomoheko <dekirumadeyamenai@gmail.com>
2020-11-06 18:07:26 +0900

    商品出品時のプレビュー機能実装

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,3 +1,27 @@
-document.addEventListener('DOMContentLoaded', function(){
-  console.log(123);
-});
+if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
+  document.addEventListener('DOMContentLoaded', function(){
+    const ImageList = document.getElementById('image-list');
+
+    const createImageHTML = (blob) => {
+      const imageElement = document.createElement('div');//<div></div>を生成
+
+      const blobImage = document.createElement('img');// <input>を生成
+      blobImage.setAttribute('src', blob);
+
+      imageElement.appendChild(blobImage);
+      ImageList.appendChild(imageElement);
+    };
+
+    document.getElementById('image-list').addEventListener('change', function(e){
+      const imageContent = document.querySelector('img');
+      if (imageContent){
+        imageContent.remove();
+      }
+
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+
+      createImageHTML(blob);
+    });
+  });
+}
